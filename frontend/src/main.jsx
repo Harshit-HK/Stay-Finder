236874bfa1f2d32ae8689ae6ff334fa98d43e74d
Harshit-HK <harshitkeshari0011@gmail.com>
2025-06-23 15:04:10 +0530

    your message here

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,33 +2,28 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import AppContextProvider from "./context/AppContext";
-// import { toast } from "react-toastify";
+import { toast } from "react-toastify";
 
-// const loadMapScript = async() => {
+const loadMapScript = async() => {
 
-//   const script = document.createElement("script");
+  const script = document.createElement("script");
 
-//   const googleKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
-//   const goMapsKey = import.meta.env.VITE_GOMAPS_API_KEY;
-//   const goMapsBase = import.meta.env.VITE_GOMAPS_BASE;
+  const googleKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 
-//   if (goMapsKey && goMapsBase) {
-//     script.src = `${goMapsBase}?key=${goMapsKey}&libraries=places`;
-//     console.log("Map is trigted")
-//   } else if (googleKey) {
-//      script.src = `${goMapsBase}?key=${googleKey}`;
-//   } else {
-//     toast.error("No Maps API key provided!");
-//     return;
-//   }
+if (googleKey) {
+     script.src = `https://maps.googleapis.com/maps/api/js?key=${googleKey}&libraries=places`;
+  } else {
+    toast.error("No Maps API key provided!");
+    return;
+  }
 
-//   script.defer = true;
-//   script.async = true;
-//   script.id = "maps-script";
-//   document.head.appendChild(script);
-// };
+  script.defer = true;
+  script.async = true;
+  script.id = "maps-script";
+  document.head.appendChild(script);
+};
 
-// loadMapScript();
+loadMapScript();
 
 createRoot(document.getElementById("root")).render(
   <AppContextProvider>
